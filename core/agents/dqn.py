97fea7050145207b92f48b3ee319862b628bcbaa
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2018-11-11 16:34:13 +0100

    :sparkles: #4 Add memory + learning skill -> agent

--- core/agents/dqn.py
+++ core/agents/dqn.py
@@ -1,12 +1,17 @@
 from core.agents.agent import Agent
 import random
 import torch
-import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+
 import numpy as np
+from core.memories.replaybuffer import ReplayBuffer
 
 
 class MLPAgent(Agent):
-    def __init__(self, agent_params, state_size, action_size, model_prototype):
+    def __init__(
+        self, agent_params, state_size, action_size, model_prototype, memory_prototype
+    ):
         super(MLPAgent, self).__init__("MLP Agent", agent_params)
 
         self.action_dim = action_size
@@ -16,42 +21,71 @@ class MLPAgent(Agent):
         # Q-Network
         self.model = model_prototype(self.model_params).to(self.device)
         self.target_model = model_prototype(self.model_params).to(self.device)
-        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+        self.optimizer = self.optim(self.model.parameters(), lr=self.lr)
 
         self._update_target_model()
 
+        # Memory
+        self.memory = memory_prototype(self.memory_params)
+
         self.counter_steps = 0
+        self.seed = random.seed(agent_params.seed)
 
     def step(self, state, action, reward, next_state, done):
-        pass
+        self.memory.append(state, action, reward, next_state, done)
+
+        if self.counter_steps >= self.learn_start and self.counter_steps % self.learn_every == 0:
+            self.learn()
+
+        self.counter_steps += 1
 
     def act(self, state):
         observation = state
 
         if self.training and self.counter_steps < self.learn_start:
             action = random.randrange(self.action_dim)
-        else:
+
+        elif self.training and self.counter_steps >= self.learn_start:
             action = self._epsilon_greedy(observation)
+        else:
+            action = self._get_action(observation)
 
         return action
 
-    def learn(self, experiences):
-        pass
+    def learn(self):
+        if self.counter_steps >= self.learn_start:
+            experiences = self.memory.sample(self.batch_size)
+            states, actions, rewards, next_states, dones = experiences
+
+            Q_targets_next = (
+                self.target_model(next_states).detach().max(1)[0].unsqueeze(1)
+            )
+            Q_targets = rewards + (self.gamma * Q_targets_next * (1 - dones))
+
+            Q_expected = self.model(states).gather(1, actions)
+
+            loss = F.mse_loss(Q_expected, Q_targets)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            self._soft_update_target_model()
 
     def _update_target_model(self):
         self.target_model.load_state_dict(self.model.state_dict())
 
-    def _epsilon_greedy(self, observation):
-        if self.training:
-            self.eps = (
-                self.eps_end
-                + max(0, (self.eps_start - self.eps_end))
-                * (self.eps_decay - max(0, self.step - self.learn_start))
-                / self.eps_decay
+    def _soft_update_target_model(self):
+        for target_param, local_param in zip(
+            self.target_model.parameters(), self.model.parameters()
+        ):
+            target_param.data.copy_(
+                self.tau * local_param.data + (1.0 - self.tau) * target_param.data
             )
-        else:
-            self.eps = self.eps_eval
 
+    def update_epsilon(self):
+        self.eps = max(self.eps_end, self.eps*self.eps_decay)
+
+    def _epsilon_greedy(self, observation):
         if np.random.uniform() < self.eps:
             action = random.randrange(self.action_dim)
 
@@ -61,8 +95,9 @@ class MLPAgent(Agent):
         return action
 
     def _get_action(self, observation):
-        observation = torch.from_numpy(np.array(state)).unsqueeze(0).to(self.device)
-        q_values = self.model(Variable(observation, volatile=True)).data
+        observation = torch.from_numpy(np.array(observation)).unsqueeze(0).to(self.device)
+        with torch.no_grad():
+            q_values = self.model(observation).data
 
         if self.use_cuda:
             action = np.argmax(q_values.cpu().numpy())
