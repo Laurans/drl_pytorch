3a20f0913b10881211d333d6fc671c43768a5be7
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2018-11-11 16:33:11 +0100

    :fire: #4 Simplify MLP model

--- core/models/dqn.py
+++ core/models/dqn.py
@@ -12,12 +12,8 @@ class QNetwork(Model):
         self.fc1 = nn.Linear(
             np.prod(self.input_dims_0 * self.input_dims_1), self.hidden_dim
         )
-        self.rl1 = nn.ReLU()
         self.fc2 = nn.Linear(self.hidden_dim, self.hidden_dim)
-        self.rl2 = nn.ReLU()
-        self.fc3 = nn.Linear(self.hidden_dim, self.hidden_dim)
-        self.rl3 = nn.ReLU()
-        self.fc4 = nn.Linear(self.hidden_dim, self.output_dims)
+        self.fc3 = nn.Linear(self.hidden_dim, self.output_dims)
 
         self.print_model()
         self.reset()
@@ -32,14 +28,8 @@ class QNetwork(Model):
         self.fc3.weight.data = nn.init.kaiming_normal_(
             self.fc3.weight.data, mode="fan_out", nonlinearity="relu"
         )
-        self.fc4.weight.data = nn.init.kaiming_normal_(
-            self.fc4.weight.data, mode="fan_out", nonlinearity="relu"
-        )
 
     def forward(self, x):
-        x = x.view(x.size(0), self.input_dims_0 * self.input_dims_1)
-        x = self.rl1(self.fc1(x))
-        x = self.rl2(self.fc2(x))
-        x = self.rl3(self.fc3(x))
-
-        return self.fc4(x.view(x.size(0), -1))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
