cb133ef618b2ad3f008100e86fc16ebe26feb6d5
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2018-11-19 23:00:04 +0100

    :sparkles: #5 Add colored logs in terminal

--- core/utils/logger.py
+++ core/utils/logger.py
@@ -1,4 +1,4 @@
-import logging
+import logging, coloredlogs
 from logging import Logger
 
 
@@ -6,11 +6,12 @@ def loggerConfig(
     log_file: str, verbose: int, namelogger: str = "drl_pytorch"
 ) -> Logger:
     logger = logging.getLogger(namelogger)
+    fmt = "[%(levelname)-8s] (%(module)s - %(funcName)s) %(message)s"
+
     logger.propagate = 0
     if not logger.handlers:
-        formatter = logging.Formatter(
-            "[%(levelname)-8s] (%(module)s - %(funcName)s) %(message)s"
-        )
+
+        formatter = logging.Formatter(fmt)
 
         fileHandler = logging.FileHandler(log_file, "w")
         fileHandler.setFormatter(formatter)
@@ -22,8 +23,10 @@ def loggerConfig(
 
         if verbose >= 2:
             logger.setLevel(logging.DEBUG)
+            coloredlogs.install(logger=logger, fmt=fmt)
         elif verbose >= 1:
             logger.setLevel(logging.INFO)
+            coloredlogs.install(logger=logger, fmt=fmt)
         else:
             streamhandler.setLevel(logging.CRITICAL)
             fileHandler.setLevel(logging.INFO)
