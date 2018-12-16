003165303fb462c1e27df091977f126fae1d6369
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2018-12-16 16:46:01 +0100

    :white_check_mark: Adding test on gym environment

--- tests/test_env_gym.py
+++ tests/test_env_gym.py
@@ -14,3 +14,19 @@ def env_gym():
 
 def test_env_state_shape(env_gym):
     assert env_gym.get_state_shape() == (8,)
+
+
+def test_env_reset(env_gym):
+    state = env_gym.reset()
+    assert state.sum() == pytest.approx(1.3239913, 0.001)
+
+
+def test_env_step(env_gym):
+    env_gym.reset()
+    s2, r, d = env_gym.step(0)
+    assert s2.sum() == pytest.approx(1.292151, 0.001)
+
+
+def test_env_render(env_gym):
+    img = env_gym.render()
+    assert img.ndim == 3
